06bd0f14a266d47952e2b8125c3ba17fa9491842
Joslin Some <joslin-some@uiowa.edu>
2023-02-11 16:54:18 -0600

    face recognition

---
+++ OpenCV/simple_facerec.py
@@ -0,0 +1,66 @@
+import face_recognition
+import cv2
+import os
+import glob
+import numpy as np
+
+class RecognitionHelper:
+    def __init__(self):
+        self.names = []
+        self.encodings = []
+        # Resize frame for a faster speed
+        self.resizedFrame = 0.25
+
+    def load_images(self, images_path):
+
+        # Loading images from folder
+        images_path = glob.glob(os.path.join(images_path, "*.*"))
+
+        print("{} encoding images found.".format(len(images_path)))
+
+        # Store image encoding and names
+        for img_path in images_path:
+            img = cv2.imread(img_path)
+            rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+            # Get the filename only from the initial file path.
+            basename = os.path.basename(img_path)
+            (filename, ext) = os.path.splitext(basename)
+            # Get encoding
+            img_encoding = face_recognition.face_encodings(rgb_img)[0]
+
+            # Store file name and file encoding
+            self.encodings.append(img_encoding)
+            self.names.append(filename)
+        print("Encoding images loaded")
+
+    def detect_known_faces(self, frame):
+        small_frame = cv2.resize(frame, (0, 0), fx=self.resizedFrame, fy=self.resizedFrame)
+        # Find all the faces and face encodings in the current frame of video
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+        face_names = []
+        for face_encoding in face_encodings:
+            # See if the face is a match for the known face(s)
+            matches = face_recognition.compare_faces(self.encodings, face_encoding)
+            name = "Unknown"
+
+            # # If a match was found in known_face_encodings, just use the first one.
+            # if True in matches:
+            #     first_match_index = matches.index(True)
+            #     name = known_face_names[first_match_index]
+
+            # Or instead, use the known face with the smallest distance to the new face
+            face_distances = face_recognition.face_distance(self.encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = self.names[best_match_index]
+            face_names.append(name)
+
+        # Convert to numpy array to adjust coordinates with frame resizing quickly
+        face_locations = np.array(face_locations)
+        face_locations = face_locations / self.resizedFrame
+        return face_locations.astype(int), face_names
