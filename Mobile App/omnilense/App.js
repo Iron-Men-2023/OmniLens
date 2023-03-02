bb1ae1aab8587b0fa357ad6b5b8c6564fa70b369
Ben DeSollar <ben.desollar01@gmail.com>
2023-03-01 18:11:42 -0600

    Added to ML screen.

--- Mobile App/omnilense/App.js
+++ Mobile App/omnilense/App.js
@@ -1,14 +1,12 @@
 import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Navigation from "./routes/Navigation"
-
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View} from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
+import Navigation from './routes/Navigation';
+import * as Sentry from 'sentry-expo';
 
 export default function App() {
-  return (
-    <Navigation/>
-  );
+  return <Navigation />;
 }
 
 const styles = StyleSheet.create({
--- Mobile App/omnilense/screens/MLScreen/MLScreen.js
+++ Mobile App/omnilense/screens/MLScreen/MLScreen.js
@@ -15,6 +15,7 @@ const FaceRecognitionExample = () => {
   const [person, setPerson] = useState(null);
   const [personIsSet, setPersonIsSet] = useState(false);
   const [cameraOn, setCameraOn] = useState(true);
+  const [pictureUploading, setPictureUploading] = useState(false);
   // Turn on the camera when the component mounts
 
   const turnOffCamera = async () => {
@@ -35,65 +36,75 @@ const FaceRecognitionExample = () => {
   // Handle face detection events
   const handleFacesDetected = async ({faces, camera}) => {
     console.log('Faces detected:', faces);
-
-    async function recognizeFace(imageUri) {
-      console.log('imageUri', imageUri);
-      const localUri = imageUri;
-
-      const response = await fetch(localUri);
-      const blob = await response.blob();
-      const userId = auth.currentUser.uid;
-      const path = `images/ml_images/${userId}.jpg`;
-      const task = storage.ref(path).put(blob);
-      task.on(
-        'state_changed',
-        snapshot => {
-          const progress =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log(`Upload is ${progress}% done`);
-        },
-        error => {
-          console.log(error);
-          Alert.alert('An error occurred while uploading the photo.');
-        },
-        async () => {
-          console.log('Upload complete');
-          const uri = await storage.ref(path).getDownloadURL();
-          const formData = new FormData();
-          formData.append('path', path);
-          console.log('formData', formData);
-          try {
-            const response = await fetch(
-              'http://192.168.0.203:8000/api/facial-recognition',
-              {
-                method: 'POST',
-                body: formData,
-              },
-            );
-            const data = await response.json();
-            console.log('data', data);
-            if (data.message === 'No face found') {
-              console.log('No face found');
-              return 'No face found';
+    try {
+      async function recognizeFace(imageUri) {
+        console.log('imageUri', imageUri);
+        const localUri = imageUri;
+        setPictureUploading(true);
+        const response = await fetch(localUri);
+        const blob = await response.blob();
+        const userId = auth.currentUser.uid;
+        const path = `images/ml_images/${userId}.jpg`;
+        const task = storage.ref(path).put(blob);
+        task.on(
+          'state_changed',
+          snapshot => {
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log(`Upload is ${progress}% done`);
+          },
+          error => {
+            console.log(error);
+            Alert.alert('An error occurred while uploading the photo.');
+          },
+          async () => {
+            console.log('Upload complete');
+            const uri = await storage.ref(path).getDownloadURL();
+            const formData = new FormData();
+            formData.append('path', path);
+            console.log('formData', formData);
+            try {
+              const response = await fetch(
+                'http://192.168.0.203:8000/api/facial-recognition',
+                {
+                  method: 'POST',
+                  body: formData,
+                },
+              );
+              const data = await response.json();
+              console.log('data', data);
+              if (data.message === 'No face found') {
+                console.log('No face found');
+                return 'No face found';
+              }
+              console.log('data.predicted_person', data.predicted_person);
+              setPerson(data.predicted_person[0]);
+              setPersonIsSet(true);
+              setPictureUploading(false);
+              return data.predicted_person[0];
+            } catch (e) {
+              console.log('No person found OR Need to start server', e);
             }
-            console.log('data.predicted_person', data.predicted_person);
-            setPerson(data.predicted_person[0]);
-            setPersonIsSet(true);
-            return data.predicted_person[0];
-          } catch (e) {
-            console.log('No person found OR Need to start server', e);
-          }
-          return 'No person found OR Need to start server';
-        },
-      );
-    }
+            return 'No person found OR Need to start server';
+          },
+        );
+      }
 
-    if (faces.length > 0) {
-      const photo = await camera.takePictureAsync();
-      console.log('Photo taken:');
-      recognizeFace(photo.uri).then(r => console.log(r));
-    } else {
-      console.log('No faces detected');
+      if (faces.length > 0) {
+        if (pictureUploading) {
+          console.log('Picture is uploading');
+          return;
+        }
+        const photo = await camera.takePictureAsync();
+        console.log('Photo taken:');
+        cameraRef.pausePreview();
+        recognizeFace(photo.uri).then(r => console.log(r));
+        cameraRef.resumePreview();
+      } else {
+        console.log('No faces detected');
+      }
+    } catch (e) {
+      console.log('Error', e);
     }
   };
 
@@ -104,6 +115,10 @@ const FaceRecognitionExample = () => {
   };
 
   function toggleCameraType() {
+    if (pictureUploading) {
+      console.log('Picture is uploading');
+      return;
+    }
     setType(current =>
       current === CameraType.back ? CameraType.front : CameraType.back,
     );
@@ -128,18 +143,16 @@ const FaceRecognitionExample = () => {
           type={type}
           ref={handleCameraReady}
           onCameraReady={requestPermission}
-          onMountError={handleFaceDetectionError}
           onFacesDetected={({faces}) =>
             handleFacesDetected({faces, camera: cameraRef})
           }
           onFaceDetectionError={handleFaceDetectionError}
           quality={1} // adjust quality
-          ratio="16:9" // adjust aspect ratio
           faceDetectorSettings={{
-            mode: FaceDetector.FaceDetectorMode.fast,
-            detectLandmarks: FaceDetector.FaceDetectorLandmarks.none,
-            runClassifications: FaceDetector.FaceDetectorClassifications.none,
-            minDetectionInterval: 10000,
+            mode: FaceDetector.FaceDetectorMode.accurate,
+            detectLandmarks: FaceDetector.FaceDetectorLandmarks.all,
+            runClassifications: FaceDetector.FaceDetectorClassifications.all,
+            minDetectionInterval: 5000,
             tracking: true,
           }}>
           <View style={styles.buttonContainer}>
@@ -151,7 +164,9 @@ const FaceRecognitionExample = () => {
       ) : (
         <Text>Camera is off</Text>
       )}
-      <View>{personIsSet && <Text>Person: {person}</Text>}</View>
+      <View style={styles.personContainer}>
+        {personIsSet && <Text style={styles.personText}>Person: {person}</Text>}
+      </View>
     </View>
   );
 };
@@ -176,7 +191,7 @@ const styles = StyleSheet.create({
     alignSelf: 'flex-end',
   },
   text: {
-    fontSize: 18,
+    fontSize: 40,
     color: 'white',
   },
   toggleContainer: {
@@ -190,6 +205,14 @@ const styles = StyleSheet.create({
   toggleText: {
     color: '#fff',
     fontWeight: 'bold',
+  },
+  personContainer: {
+    flex: 0.1,
+  },
+  personText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 20,
   },
 });
 export default FaceRecognitionExample;
